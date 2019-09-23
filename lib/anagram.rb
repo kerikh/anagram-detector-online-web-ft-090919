0935e06ad7a62a5de1f93c5f7ffd902c7f25bfba
kerikh <kerid18@att.net>
2019-09-22 20:36:30 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_words)
+    sorted_word = @word.chars.sort
+    possible_words.select { |possible_word| possible_word.chars.sort == sorted_word }
+  end
+end
